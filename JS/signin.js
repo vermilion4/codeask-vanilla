62d6873cfdcb7c6576fa9c1964f5664a1e3d4356
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-09-13 07:25:05 -0700

    Error UI implemented

--- JS/signin.js
+++ JS/signin.js
@@ -22,42 +22,102 @@ form.addEventListener('submit', function (event) {
 //COLLECT DATA
 function collectData(){
 
-          let  email =  document.getElementById('email').value;
-          let  password =  document.getElementById('password').value;
+    let  email =  document.getElementById('email').value;
+    let  password =  document.getElementById('password').value;
 
 
-         if(localStorage.getItem("users") === null){
+    if(localStorage.getItem("users") === null){
 
+    }else{
+        let returnKey;
+
+    for(let i = 0; i < JSON.parse(localStorage.getItem("users")).length; i++){
+        
+        allElements = JSON.parse(localStorage.getItem("users"));
+        let userEmail = JSON.stringify(allElements[i]["email"]).slice(1,-1);
+        let userPassword = JSON.stringify(allElements[i]["password"]).slice(1,-1);    
+        
+        if(email === userEmail && password === userPassword){
+            
+            returnKey = "success";
+            break;
+
+        }else if(email === "" || password === ""){  // if they exist
+            
+            returnKey = "noinput";
         }else{
-            let returnKey;
-    
+
+            returnKey = "failed";
+        }
+    } 
+    return returnKey ;
+    }
+}
+
+function validateEmail(){
+    let  email =  document.getElementById('email').value;
+    const typeInput = "email";
+
+
+   if(localStorage.getItem("users") === null){
+
+   }else{
+
         for(let i = 0; i < JSON.parse(localStorage.getItem("users")).length; i++){
             
+            allElements = JSON.parse(localStorage.getItem("users"));
+            let userEmail = JSON.stringify(allElements[i]["email"]).slice(1,-1);   
+            
+            if(email === userEmail){
+
+                errorMessage("", typeInput);
+                inputFieldES(typeInput, true);
+                return false;
+        
+            }else{
+                errorMessage("Email does not match", typeInput);
+                inputFieldES(typeInput,false);
+                return true;
+            }
+        }
+    }
+}
+
+function validatePassword(){
+    let  password =  document.getElementById('password').value;
+    const typeInput = "password";
+
+  if(localStorage.getItem("users") === null){
+
+  }else{
+
+
+        for(let i = 0; i < JSON.parse(localStorage.getItem("users")).length; i++){
+        
             allElements = JSON.parse(localStorage.getItem("users"));
             let userEmail = JSON.stringify(allElements[i]["email"]).slice(1,-1);
             let userPassword = JSON.stringify(allElements[i]["password"]).slice(1,-1);    
             
-            if(email === userEmail && password === userPassword){
-                
-                returnKey = "success";
-                break;
-    
-            }else if(email === "" || password === ""){  // if they exist
-                
-                returnKey = "noinput";
+            if( password === userPassword){
+                errorMessage("", typeInput);
+                inputFieldES(typeInput, true);
+                return true;
             }else{
-
-                returnKey = "failed";
+               
+                errorMessage("Password does not match", typeInput);
+                inputFieldES(typeInput, false);
+                return false;
             }
-        } 
-        return returnKey ;
         }
+    }
 }
+
   
 function errorMessage(info,element){
 
     if(element){
-        const errorBox = document.querySelector(".error-message");
+        const errorField = `#${element}~.error-message`;
+        const errorBox = document.querySelector(errorField);
 
         errorBox.innerText = info;
         errorBox.style.display = "block";
@@ -71,12 +131,33 @@ function errorMessage(info,element){
 
 function verify(){
     if(collectData() === "success"){
-        alert("login In succesful")
-        window.location = "construction.html";  //will check and change page
+        window.location = "question.html";  //will check and change page
     }
     if(collectData() === "failed"){
 
-        errorMessage("Incorrect login details")
+        // errorMessage("Incorrect login details")
+    }
+}
+
+
+//Error and Success for Input field 
+function inputFieldES(element,bool){
+   
+    const inputField = `#${element}`;
+    const inputBox = document.querySelector(inputField);
+    const inputIcon = document.querySelector(`${inputField}~.bi`);
+
+    if(bool === false){
+   
+        inputBox.style.border = "1px solid #DC3545";
+        inputIcon.classList.add("bi-exclamation-circle");
+        inputIcon.classList.remove("bi-check");
+
+    }else{
+
+        inputBox.style.border = "1px solid #198754";
+        inputIcon.classList.add("bi-check");
+        inputIcon.classList.remove("bi-exclamation-circle");
     }
 }
 
