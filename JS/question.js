98b50639b362384ac1edf70900ce6d166b2d299a
Clement Adeyemi <adeyemiclement1@gmail.com>
2022-09-15 04:22:58 +0200

    Question page ready for your code, sidebar bug fixed

--- JS/question.js
+++ JS/question.js
@@ -1,23 +1,25 @@
-// JAVASCRIPT FOR SIDEBAR
-function hideIconFunction() {
- let iconFunction = document.querySelectorAll('.iconFunction');
- // for (let i = 0; i < iconFunction.length; i++) {
- //  iconFunction[i].style.display = 'none';
- // }
- iconFunction.forEach(icon => {
- icon.style.display = 'none';
-});
+// VARIABLES
+const iconFunction = document.querySelectorAll('.iconFunction');
+const upperIcons = document.getElementById('upperIcons');
+const sideBar = document.getElementById('sideBar');
+const questionPage = document.getElementById('questionPage');
 
-document.getElementById('upperIcons').style.alignItems = 'center';
+// JAVASCRIPT FOR SIDEBAR
+sideBar.onmouseover = function() {
+  for (let i = 0; i < iconFunction.length; i++) {
+      iconFunction[i].style.display = 'block';
+  }
+  upperIcons.style.alignItems = 'start';
+  questionPage.style.width = 'calc(59% - 76px)';
+  questionPage.style.marginLeft = 'calc(120px + 76px)';
+  
 }
 
-function displayIconFunction() {
- let iconFunction = document.querySelectorAll('.iconFunction');
- iconFunction.forEach(icon => {
-  setTimeout(() => {
-   icon.style.display = 'flex';
-  }, 200)
-});
-
-document.getElementById('upperIcons').style.alignItems = 'start';
+sideBar.onmouseout = function() {
+  for (let i = 0; i < iconFunction.length; i++) {
+      iconFunction[i].style.display = 'none';
+    }
+    upperIcons.style.alignItems = 'center';
+    questionPage.style.width = '59%';
+    questionPage.style.marginLeft = '120px';
 }
