ca53726e96156a26a899bbd9acb43cf8196145fa
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-09-28 00:21:51 +0100

    Set Questions

--- JS/askquestion.js
+++ JS/askquestion.js
@@ -3,3 +3,19 @@ function closeModal(){
     const modal = document.querySelector(".askquestion-modal");
     modal.style.display = "none";
 }
+
+let questionsList = JSON.parse(localStorage.getItem("question"))  ? JSON.parse(localStorage.getItem("question")) : [];
+
+function getQuestion(){
+    let eachQuestion ={
+        "title" : document.querySelector(".title input").value,
+        "body" : document.querySelector(".body textarea").value,
+        "tags" : document.querySelector(".tags input").value
+    }
+
+
+    questionsList.push(eachQuestion);
+    localStorage.setItem("question", JSON.stringify(questionsList));
+    window.location = "question.html";
+}
+
