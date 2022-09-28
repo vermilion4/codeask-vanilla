b6dd5416cd06988cd6a60e94255a3107c9ffc5dd
Adaeze Ndupu <andupu4@gmail.com>
2022-09-28 19:46:23 +0100

    Question UI fix

--- JS/askquestion.js
+++ JS/askquestion.js
@@ -1,25 +1,27 @@
 
-function closeModal(){
+function closeModal ()
+{
     const modal = document.querySelector(".askquestion-modal");
     modal.style.display = "none";
 }
 
-let questionsList = JSON.parse(localStorage.getItem("question"))  ? JSON.parse(localStorage.getItem("question")) : [];
-let singedInUser = JSON.parse(localStorage.getItem("session"))
+let questionsList = JSON.parse(localStorage.getItem("question")) ? JSON.parse(localStorage.getItem("question")) : [];
+let singedInUser = JSON.parse(localStorage.getItem("session"));
 
-function getQuestion(){
-    const {name} = singedInUser
-    
-    let eachQuestion ={
-        "post_id":"",
-        "author_id":"",
+function getQuestion ()
+{
+    const { name } = singedInUser;
+
+    let eachQuestion = {
+        "post_id": "",
+        "author_id": "",
         "author": name,
-        "title" : document.querySelector(".title input").value,
-        "body" : document.querySelector(".body textarea").value,
-        "tags" : document.querySelector(".tags input").value,
-        "date_added": new Date.now(),
-        'answer' : []
-    }
+        "title": document.querySelector(".title input").value,
+        "body": document.querySelector(".body textarea").value,
+        "tags": document.querySelector(".tags input").value,
+        "date_added": Date.now(),
+        'answer': []
+    };
 
 
     questionsList.push(eachQuestion);
