35eb600d25866afbde5ae34a13f97c1f09cb1afb
Nweke Chidi <nwekechidigodwin460@gmail.com>
2022-10-10 12:45:06 +0100

    Updated all changes

--- JS/shared.js
+++ JS/shared.js
@@ -3,8 +3,11 @@ function hamburger ()
   let menu = document.querySelector(".mobile-nav-menu");
   let hamburgerStroke = document.querySelector(".mobile-nav");
 
-  menu.style.display === "block" ? menu.style.display = "none" : menu.style.display = "block";
+  // menu.style.display === "block" ? menu.style.display = "none" : menu.style.display = "block";
+ 
+     menu.classList.toggle("drop");
 
+ 
   // hamburgerStroke.classList.toggle("open");
 }
 
